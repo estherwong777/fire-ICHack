f4b643a57ec3c76b134d49b3c46af582e1ed80fc
Esther Wong <majesticyak@dyn1237-162.wlan.ic.ac.uk>
2018-01-27 19:38:20 +0000

    player file updated

--- app/src/main/java/group43/fire/Player.java
+++ app/src/main/java/group43/fire/Player.java
@@ -1,20 +1,26 @@
 package group43.fire;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
 /**
  * Created by jasmi on 27/01/2018.
  */
 
 public class Player {
 
-//<<<<<<< HEAD
+  //<<<<<<< HEAD
   private Position posit;
+  private Bitmap playerImage;
 
-  public Player(){
-    this.posit = new Position();
+  public Player(Bitmap playerImage){
+    this.posit       = new Position();
+    this.playerImage = playerImage;
   }
 
   public Player (int start){
-    this.posit = new Position(start, 0);
+    this.posit       = new Position(start, 0);
+    this.playerImage = playerImage;
   }
 
   public void moveRight(){
@@ -37,7 +43,8 @@ public class Player {
     return posit.getY();
   }
 
+  public void draw(Canvas canvas) {
+    canvas.drawBitmap(playerImage, getPlayerX(), getPlayerY(), null);
+  }
+
 }
-
-
-
