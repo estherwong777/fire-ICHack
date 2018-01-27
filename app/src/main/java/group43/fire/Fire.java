04aef902a3a9cc0b016d9194d88fd7c16b32c8a0
Yiming <yx3017@ic.ac.uk>
2018-01-27 15:41:58 +0000

    Four classes added

--- app/src/main/java/group43/fire/Fire.java
+++ app/src/main/java/group43/fire/Fire.java
@@ -4,5 +4,27 @@ package group43.fire;
  * Created by jasmi on 27/01/2018.
  */
 
-public class Fire {
+public class Fire  {
+  private Position position;
+  private Player player;
+
+  public Fire() {
+    this.position = new Position(player.getPlayerX(), player.getPlayerY() - 1);
+  }
+
+  public Fire(Position position){
+    this.position = position;
+  }
+  public void move() {
+    this.position = new Position(getFireX(), getFireY() - 1);
+
+  }
+
+  public int getFireX(){
+    return position.getX();
+  }
+  public int getFireY(){
+    return position.getY();
+
+  }
 }
--- app/src/main/java/group43/fire/Player.java
+++ app/src/main/java/group43/fire/Player.java
@@ -5,4 +5,35 @@ package group43.fire;
  */
 
 public class Player {
+
+  private Position posit;
+
+  public Player(){
+    this.posit = new Position();
+  }
+
+  public Player (int start){
+    this.posit = new Position(start, 0);
+  }
+
+  public void moveRight(){
+    this.posit = new Position(posit.getX()+1,0);
+  }
+
+  public void moveLeft(){
+    this.posit = new Position(posit.getX()-1,0);
+  }
+
+  public Position getPlayerPosition(){
+    return posit;
+  }
+
+  public int getPlayerX(){
+    return posit.getX();
+  }
+
+  public int getPlayerY(){
+    return posit.getY();
+  }
+
 }
